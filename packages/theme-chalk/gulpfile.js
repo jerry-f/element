40fc55b87182185769ef1a05c1188c58e17d68ca
break-wave <568336789@qq.com>
2019-01-24 15:29:32 +0800

    study mixins and function

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -16,6 +16,32 @@ gulp.task('compile', function() {
     .pipe(gulp.dest('./lib'));
 });
 
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.scss', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    return gulp.src('./src/*.scss')
+      .pipe(sass.sync({
+        outputStyle: 'expanded' // nested/expanded/compact/compressed
+      }).on('error', sass.logError))
+      // .pipe(autoprefixer({
+      //   browsers: ['ie > 9', 'last 2 versions'],
+      //   cascade: false
+      // }))
+      // .pipe(cssmin())
+      .pipe(gulp.dest('./lib'));
+    // return gulp.src(event.path)
+    //   .pipe(sass.sync({
+    //     outputStyle: 'expanded' // nested/expanded/compact/compressed
+    //   }).on('error', sass.logError))
+    //   .pipe(autoprefixer({
+    //     browsers: ['ie > 9', 'last 2 versions'],
+    //     cascade: false
+    //   }))
+    //   // .pipe(cssmin())
+    //   .pipe(gulp.dest('./lib'));
+  });
+});
+
 gulp.task('copyfont', function() {
   return gulp.src('./src/fonts/**')
     .pipe(cssmin())
